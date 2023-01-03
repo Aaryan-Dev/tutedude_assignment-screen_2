40e8e930c3bb0d77cc86298cb89ad9eb350589e6
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2023-01-04 04:21:42 +0530

    Update EnrolledFriends.jsx

--- src/miniComponents/EnrolledFriends.jsx
+++ src/miniComponents/EnrolledFriends.jsx
@@ -2,24 +2,7 @@ import React from "react";
 import css from "../my.module.css";
 
 const EnrolledFriends = () => {
-  return (
-    <div className={css.enroll}>
-      <div className={css.code}>
-        <div className={css.referral}>Your Referral Code</div>
-
-        <div className={css.coder}>
-          <pre>EDCH54</pre>
-        </div>
-      </div>
-
-      <div className={css.wall}>
-        <div className={css.wallet}>
-          <pre>Wallet Balance</pre>
-        </div>
-        <div className={css.rupee}>&#8377; 500 </div>
-      </div>
-    </div>
-  );
+  return <div className={css.enroll}></div>;
 };
 
 export default EnrolledFriends;
