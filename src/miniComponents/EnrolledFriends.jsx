19d830644fcf96b783340f6991d7c67b32da4e30
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2023-01-04 06:39:35 +0530

    almost

--- src/miniComponents/EnrolledFriends.jsx
+++ src/miniComponents/EnrolledFriends.jsx
@@ -35,28 +35,40 @@ const EnrolledFriends = () => {
       <div id={css.how}>
         <div id={css.grid_it}>
           <div>
-            <img src="img" alt="info" />
+            <div>
+              <img src="https://i.postimg.cc/Hk5GQBFc/invite.png" alt="info" />
+            </div>
+
             <div>
               <div>Invite your Friends</div>
               <div>Share the link tutedude.com with your friends</div>
             </div>
           </div>
           <div>
-            <img src="img" alt="info" />
+            <div>
+              <img
+                src="https://i.postimg.cc/KYy6Qty9/purchase.png"
+                alt="info"
+              />
+            </div>
             <div>
               <div>Friend purchases any course</div>
               <div>Using your REFERRAL CODE in the payments page</div>
             </div>
           </div>
           <div>
-            <img src="img" alt="info" />
+            <div>
+              <img src="https://i.postimg.cc/5tqZrj4f/rupee.png" alt="info" />
+            </div>
             <div>
               <div>You get ₹ 200 as referral money</div>
               <div>On total purchase the friend makes, into your wallet</div>
             </div>
           </div>
           <div>
-            <img src="img" alt="info" />
+            <div>
+              <img src="https://i.postimg.cc/xC6B07N7/off.png" alt="info" />
+            </div>
             <div>
               <div>Your Friend gets ₹ 200 Off</div>
               <div>
@@ -66,7 +78,9 @@ const EnrolledFriends = () => {
             </div>
           </div>
           <div>
-            <img src="img" alt="info" />
+            <div>
+              <img src="https://i.postimg.cc/bJyKf8rk/wallet.png" alt="info" />
+            </div>
             <div>
               <div>Transfer money from wallet</div>
               <div>
